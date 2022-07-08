316d5dc63a6ea33ba48252587c878fb2c83cd371
Maksim Meliashchuk <maksym873@gmail.com>
2022-07-09 00:50:22 +0300

    configured OpenApiGeneratedServerApplication

---
+++ rest-app-openapi/src/main/java/com/disposableemail/rest/OpenApiGeneratedServerApplication.java
@@ -0,0 +1,17 @@
+package com.disposableemail.rest;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiGeneratedServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiGeneratedServerApplication.class, args);
+	}
+
+}
---
+++ rest-app-openapi/src/test/java/com/disposableemail/rest/OpenApiGeneratedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disposableemail.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
