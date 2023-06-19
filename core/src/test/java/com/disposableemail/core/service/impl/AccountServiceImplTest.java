76452a94b4eaebb6f0171a9d86bfe4c233d488a8
Maksim Meliashchuk <maksym873@gmail.com>
2023-06-19 23:57:45 +0300

    [REFACTOR] added some AccountService tests

--- core/src/test/java/com/disposableemail/core/service/impl/AccountServiceImplTest.java
+++ core/src/test/java/com/disposableemail/core/service/impl/AccountServiceImplTest.java
@@ -2,6 +2,8 @@ package com.disposableemail.core.service.impl;
 
 import com.disposableemail.AbstractSpringIntegrationTest;
 import com.disposableemail.core.dao.entity.AccountEntity;
+import com.disposableemail.core.dao.entity.DomainEntity;
+import com.disposableemail.core.model.Credentials;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,9 +12,12 @@ import reactor.test.StepVerifier;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AccountServiceImplTest extends AbstractSpringIntegrationTest {
+
+    private final DomainEntity domain = new DomainEntity("1", "example.com", true, false);
+
     private final AccountEntity account1 = AccountEntity.builder()
             .id("1")
-            .address("account1@email.com")
+            .address("account1@example.com")
             .quota(40000)
             .used(4000)
             .mailboxId("mailboxId1")
@@ -21,7 +26,7 @@ class AccountServiceImplTest extends AbstractSpringIntegrationTest {
             .build();
     private final AccountEntity account2 = AccountEntity.builder()
             .id("2")
-            .address("account2@email.com")
+            .address("account2@example.com")
             .quota(50000)
             .used(5000)
             .mailboxId("mailboxId2")
@@ -33,16 +38,77 @@ class AccountServiceImplTest extends AbstractSpringIntegrationTest {
     public void setUp() {
         accountRepository.save(account1).block();
         accountRepository.save(account2).block();
+        domainRepository.save(domain).block();
     }
 
     @AfterEach
     public void cleanUp() {
         accountRepository.deleteAll().block();
+        domainRepository.deleteAll().block();
     }
+
+    @Test
+    void shouldCreateAccount() {
+        int size = 3;
+        var credentials = new Credentials("account3@example.com", "password");
+
+        accountService.createAccount(credentials).block();
+
+        StepVerifier
+                .create(accountRepository.findAll())
+                .expectSubscription()
+                .expectNextCount(size)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void shouldGetAccountById() {
+
+        var account = accountService.getAccountById("1");
+
+        StepVerifier
+                .create(account)
+                .expectSubscription()
+                .assertNext(acc -> {
+                    assertEquals(account1.getId(), acc.getId());
+                    assertEquals(account1.getAddress(), acc.getAddress());
+                    assertEquals(account1.getIsDeleted(), acc.getIsDeleted());
+                    assertEquals(account1.getQuota(), acc.getQuota());
+                    assertEquals(account1.getUsed(), acc.getUsed());
+                    assertEquals(account1.getMailboxId(), acc.getMailboxId());
+                    assertEquals(account1.getIsDisabled(), acc.getIsDisabled());
+                })
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void shouldGetAccountByAddress() {
+
+        var account = accountService.getAccountByAddress("account1@example.com");
+
+        StepVerifier
+                .create(account)
+                .expectSubscription()
+                .assertNext(acc -> {
+                    assertEquals(account1.getId(), acc.getId());
+                    assertEquals(account1.getAddress(), acc.getAddress());
+                    assertEquals(account1.getIsDeleted(), acc.getIsDeleted());
+                    assertEquals(account1.getQuota(), acc.getQuota());
+                    assertEquals(account1.getUsed(), acc.getUsed());
+                    assertEquals(account1.getMailboxId(), acc.getMailboxId());
+                    assertEquals(account1.getIsDisabled(), acc.getIsDisabled());
+                })
+                .expectComplete()
+                .verify();
+    }
+
 
     @Test
     void shouldFindAllAccounts() {
         int size = 2;
+
         var accounts = accountService.getAccounts(size, 0);
 
         StepVerifier
@@ -55,22 +121,18 @@ class AccountServiceImplTest extends AbstractSpringIntegrationTest {
 
     @Test
     void shouldDeleteAccount() {
+        int size = 2;
+
         accountService.deleteAccount("1").block();
-        var accounts = accountService.getAccounts(2, 0);
+
+        var accounts = accountService.getAccounts(size, 0);
 
         StepVerifier
                 .create(accounts)
                 .expectSubscription()
-                .assertNext(account -> {
-                    assertEquals(account2.getId(), account.getId());
-                    assertEquals(account2.getAddress(), account.getAddress());
-                    assertEquals(account2.getIsDeleted(), account.getIsDeleted());
-                    assertEquals(account2.getQuota(), account.getQuota());
-                    assertEquals(account2.getUsed(), account.getUsed());
-                    assertEquals(account2.getMailboxId(), account.getMailboxId());
-                    assertEquals(account2.getIsDisabled(), account.getIsDisabled());
-                })
+                .expectNextCount(size - 1)
                 .expectComplete()
                 .verify();
     }
+
 }
